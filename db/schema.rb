e2713ecdcf4f86433e3b3d5643ec7aaceb6baaf3
goofiw <goofiw@gmail.com>
2015-05-18 23:43:49 +0000

    added user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518182522) do
+ActiveRecord::Schema.define(version: 20150518232811) do
 
   create_table "songs", force: true do |t|
     t.string   "song_title"
     t.string   "author"
     t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "user"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
